9df38ece98510c34c3ebeac6d7a4119a56896237
darshankumar1992 <darshanshetty192@gmail.com>
2025-10-10 11:04:51 +0530

    dd

--- Neww.java
+++ Neww.java
@@ -2,5 +2,6 @@ public class Neww {
     public static void main(String[] args){
         System.out.println("Haii Darshan");
         System.out.println("Welcome to Welleazy");
+        System.out.println("Good Morning");
     }
 }
